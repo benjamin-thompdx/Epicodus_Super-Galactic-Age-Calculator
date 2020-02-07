768aeee8b6b0f34930472b06868e3b483202fdbe
Benjamin Thom <jaminthom@gmail.com>
2020-02-07 14:12:45 -0800

    update indentation within project.test.js file

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -1,10 +1,23 @@
-import { SuperGalc } from './../src/super-galactic-age-calculator.js';
+import { GalacticCalc } from './../src/super-galactic-age-calculator.js';
 
 //Earth Age Methods
 
-describe('SuperGalc', function() {
-  test('should create SuperGalc object', function() {
-    const dob = new SuperGalc(new Date("1990, 11, 27"));
-    expect(typeof dob).toEqual(typeof new SuperGalc);
+describe('GalacticCalc', function() {
+  
+  test('should create GalacticCalc object', function () {
+    const dob = new GalacticCalc(new Date("1990, 11, 27"));
+    expect(typeof dob).toEqual(typeof new GalacticCalc);
   });
+
+  test('should calculate age in years from a date', function () {
+
+  })
+
+  //Mercury
+
+  //Venus
+
+  //Mars
+
+  //Jupiter
 })
--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -1,4 +1,4 @@
-export class SuperGalc {
+export class GalacticCalc {
   constructor(date) {
     this.date = new Date(date);
   }
