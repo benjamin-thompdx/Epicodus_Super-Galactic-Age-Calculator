dfd0b6e217b84dc84dcb7b567f02b6f21ae6c9a3
Benjamin Thom <jaminthom@gmail.com>
2020-02-07 17:22:55 -0800

    pass test should return the number of years the user has to live based on U.S. 2020 average lifespan 78.93 for Earth in Earth years

--- __tests__/super-galactic-age-calculator.test.js
+++ __tests__/super-galactic-age-calculator.test.js
@@ -9,7 +9,7 @@ describe('GalacticCalc', function() {
   });
 
   test('should return the number of years the user has to live based on U.S. 2020 average lifespan 78.93 for Mercury in Earth years', function () {
-    expect(age.getLexEarth(29)).toEqual(500);
+    expect(age.getLexEarth(29)).toEqual(49);
   });
 
   //Mercury Calculations
